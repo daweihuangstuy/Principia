7510e1537e477bcd712f089804b423954162fc21
Chester Huang <chuang9@stuy.edu>
2017-05-25 11:25:46 -0400

    implemented tree version of topics

--- Catalog.java
+++ Catalog.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public final class Catalog{
     
     public static String[] listTopics =
@@ -6,8 +8,16 @@ public final class Catalog{
      "M3",
      "etc"};
 
-    public Tree<String> treeTopics = new Tree<String>("root", 
+    public Tree<String> treeTopics = new Tree<String>("root");
 
-    
+    public Catalog() {
+	treeTopics.addChild("Motion");
+	treeTopics.addChild("some topic");
+
+    }
+
+    public String toString() {
+	return treeTopics.getChildren().toString();
+    }
 
 }
