e273613fb89f4716dd274e9d631594049d8f718d
daweihuangstuy <dhuang9@stuy.edu>
2017-05-25 10:44:43 -0400

    changed stuff

--- Motion.java
+++ Motion.java
@@ -10,16 +10,25 @@ public class Motion{
 
 
     // subCat 1
-    public static int unDisp1(int acc, int time, int intVel){
-	return (time * 
+    public static double[] strToDoubleArr(String[] list){
+	double[] returnArr = new double[list.size];
+	for(int i = 0; i < list.size; i++){
+	    returnArr[i] = Integer.parseInt(list[i]);
+	}
+	return returnArr;
+    }
+    public static double unDisp1(double acc, double time, double intVel){
+	return (time * intVel + 0.5 * acc * time * time); 
     }
 
     public static void main(String[] args){
+	String input;
 	// interface
+	System.out.println("\nMotion");
 	System.out.println("\n1. Displacement(1)/Time(2)/Acceleration(3)/InitVelocity(4)");
-
+	
 	// option
-	choiceCMD = cs1.Keyboard.readString();
+	String choiceCMD = cs1.Keyboard.readString();
 	while(!choiceCMD.equals("1")){
 	    choiceCMD = cs1.Keyboard.readString();
 	}
@@ -27,13 +36,20 @@ public class Motion{
 	// selection
 	if (choiceCMD.equals("1")){
 	    System.out.println("Choose unknown(1, 2, 3)");
-	    unknownCMD = cs1.Keyboard.readString();
+	    String unknownCMD = cs1.Keyboard.readString();
 	    while(!unknownCMD.equals("1") ||!unknownCMD.equals("2") ||!unknownCMD.equals("3") ){
 		unknownCMD = cs1.Keyboard.readString();
 	    }
-	    if(
-	}
-	
-	
+	    // unknown category 1
+	    if (unknownCMD.equals("1")){
+		System.out.println("\nInput Time_Acceleration_InitVelocity in that order");
+		input = cs1.Keyboard.readString();
+		String[]  = input.split("\\s+");
+		System.out.println(unDisp1(
+		
+		 
+				  
+	    }
+	}	
     }
 }
