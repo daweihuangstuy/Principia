78ca9ce7eca744c32f27f7207645819d81bbc22d
Chester Huang <chuang9@stuy.edu>
2017-05-29 23:09:03 -0400

    arrays for types of equations

---
+++ Forces.java
@@ -0,0 +1,10 @@
+public static class Forces {
+
+    // [F, m, a]
+    // [W, m, g]
+    // [F, k, x]
+    // [F, us, uk, Fn]
+    // [Fcp, m, acp, v, r]
+
+
+}
--- MMotion.java
+++ MMotion.java
@@ -1,6 +1,7 @@
 public static class Motion {
 
     // [xi, xf, dx, vi, vf, dv, ai, af, da]
+    // [x0, y0, x, y, vxi, vyi, vxf, vyf, t, g, hi, theta, hmax, range] 
 
     public double displacement() {
 
